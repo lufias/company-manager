19a53dcd8b109a191920992a4c1327aa6a029c92
Saiful Azhar <saifulazr@gmail.com>
2025-06-03 14:12:58 +0800

    Vite prod host

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'localhost',
+            host: '65.21.254.61',
         },
     },
 });
